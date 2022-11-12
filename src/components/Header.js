b79957131b875cf1806e2b1817e9d7c2108f6eb6
websupergirl <nikol@thatsupergirl.com>
2022-11-11 21:21:42 -0800

    remove extra pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,7 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      margin: `0 auto`,
-      padding: `var(--space-4) var(--size-gutter)`,
-      display: `flex`,
-      alignItems: `center`,
-      justifyContent: `space-between`,
-    }}
-  >
+  <header>
     <Link
       to="/"
       style={{
