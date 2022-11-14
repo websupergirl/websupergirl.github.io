432d9fff09af0eae9a314f5ab5a3ddb3382e27ba
websupergirl <nikol@thatsupergirl.com>
2022-11-13 19:02:10 -0800

    add icons to sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const Sidebar = () => (
   <section id="sidebar">
@@ -21,11 +21,10 @@ const Sidebar = () => (
     </nav>
     <footer>
       <ul class="icons">
-        <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-        <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
-        <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-        <li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
-        <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+        <li><a href="#"><FontAwesomeIcon icon={brands('Twitter')} color="#b7eadc" /></a></li>
+        <li><a href="#"><FontAwesomeIcon icon={brands('Instagram')} color="#b7eadc" /></a></li>
+        <li><a href="#"><FontAwesomeIcon icon={brands('Github')} color="#b7eadc" /></a></li>
+        <li><a href="#"><FontAwesomeIcon icon={brands('LinkedIn')} color="#b7eadc" /></a></li>
       </ul>
     </footer>
   </section>
