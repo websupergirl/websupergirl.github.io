964449e3db9e90cdafb4d8c0b6b535613a8488a8
websupergirl <nikol@thatsupergirl.com>
2022-11-13 18:45:17 -0800

    remove unnecessary css

--- src/components/section-two.js
+++ src/components/section-two.js
@@ -2,6 +2,8 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const Two = () => (
   <section id="two">
@@ -9,12 +11,12 @@ const Two = () => (
     <h3>Things I Can Do</h3>
     <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum integer lorem ipsum dolor sit amet.</p>
     <ul class="feature-icons">
-      <li class="icon solid fa-code">Neque sit cep ante</li>
-      <li class="icon solid fa-cubes">Augue fusce morbi</li>
-      <li class="icon solid fa-book">Cras augue cep vitae</li>
-      <li class="icon solid fa-coffee">Augue enim dictum</li>
-      <li class="icon solid fa-bolt">Porttitor ante</li>
-      <li class="icon solid fa-users">Tempus risus consequat</li>
+      <li><FontAwesomeIcon icon={solid('code')} color="#64a2d0" /> Neque sit cep ante</li>
+      <li><FontAwesomeIcon icon={solid('cubes')} color="#64a2d0" /> Augue fusce morbi</li>
+      <li><FontAwesomeIcon icon={solid('book')} color="#64a2d0" /> Cras augue cep vitae</li>
+      <li><FontAwesomeIcon icon={solid('coffee')} color="#64a2d0" /> Augue enim dictum</li>
+      <li><FontAwesomeIcon icon={solid('bolt')} color="#64a2d0" /> Porttitor ante</li>
+      <li><FontAwesomeIcon icon={solid('users')} color="#64a2d0" /> Tempus risus consequat</li>
     </ul>
     </div>
   </section>
