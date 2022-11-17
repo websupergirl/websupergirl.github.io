283038748a89500ff1c809609bba0a4f4c73e5a8
websupergirl <nikol@thatsupergirl.com>
2022-11-16 23:39:59 -0800

    commit footer cleanup

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,11 @@
 import * as React from "react"
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 const Footer = () => (
   <section id="footer">
     <div class="container">
       <ul class="copyright">
-        <li><FontAwesomeIcon icon={solid('copyright')} color="#64a2d0" /> Untitled. All rights reserved.</li>
+        <li><FontAwesomeIcon icon={("fas", "copyright")} color="#64a2d0" /> Untitled. All rights reserved.</li>
       </ul>
     </div>
   </section>
